5467dde28a8bcec293e3cb2f151e216cea5173f1
kayacan <kayacan@mindbehind.com>
2020-09-03 11:23:54 +0300

    Girisimci sayfasini databaseden cek

--- src/Views/Investor/Investor.js
+++ src/Views/Investor/Investor.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import CustomBar from '../../Components/Navbar/CustomBar';
 import axios from "axios";
-import { fetch } from 'isomorphic-fetch';
-
 
 class Investor extends Component {
 
@@ -11,8 +9,23 @@ class Investor extends Component {
         super(props);
         this.state = {
             email: "",
-            startups: [["marti", "%99"], ["patatesGetir", "%50"], ["yohey", "asd"]]
+            startups: [["marti", "%99", "item3"], ["patatesGetir", "%50", "item4"], ["yohey", "asd", "item5"]],
+            Results : ""
         };
+
+        fetch("http://localhost:8080/getStartUps", {
+        method: "GET",
+      })
+      .then((resp) => {
+        return resp.json()
+      }) 
+      .then((data) => {
+        console.log(data);
+        this.setState({startups: data});
+      })
+      .catch((error) => {
+        console.log(error, "catch the hoop")
+      })
     }
 
     handlePWChange(event) {
@@ -24,28 +37,24 @@ class Investor extends Component {
 
     renderCompanies = () => {
         const { startups } = this.state;
-        console.log(startups, "asd")
+        console.log(startups, "asddd")
         return (
             <>
             {startups.map((item => {
                 console.log(item, "asd")
                 return (
-
-
                     <div class="col-md-12">
-      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">{item[1]}</strong>
-          <h3 class="mb-0">{item[0]}</h3>
-          <div class="mb-1 text-muted">Nov 12</div>
-          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
-        </div>
-      </div>
-    </div>
+                        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                            <div class="col p-4 d-flex flex-column position-static">
+                                <strong class="d-inline-block mb-2 text-primary">{item.projeAdi}</strong>
+                                <h3 class="mb-0">{item.projeAdi}</h3>
+                                <div class="mb-1 text-muted">Nov 12</div>
+                                <p class="card-text mb-auto">{item.projeMetni}</p>
+                                <a href="#" class="stretched-link">Continue reading</a>
+                            </div>
+                        </div>
+                    </div>
                 )
-
-
             })
 
 
@@ -64,12 +73,7 @@ class Investor extends Component {
             <div>
                 <CustomBar />
                 <div class="container">
-
-                  
                         <div>{this.renderCompanies()}</div>
-
-                   
-
                 </div>
             </div>
         );
