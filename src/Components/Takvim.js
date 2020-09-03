8abb74157439d5b56ee70ddb79b46cbd4539dd54
ekrem <ekrem@ekrems-MacBook-Air.local>
2020-09-03 14:53:43 +0300

    yazilar eklendi

--- src/Components/Takvim.js
+++ src/Components/Takvim.js
@@ -11,7 +11,7 @@ function Takvim() {
                 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet"/>	
                 <>
 	<div class="container bg-light">
-		<div class="row row-striped" style={{marginBottom:"10px"}}>
+	<div class="row row-striped" style={{marginBottom:"10px"}}>
 			<div class="col-2 text-right">
 				<h3 class="display-10"><span class="badge badge-secondary">14</span></h3>
 				<h5>EKM</h5>
@@ -24,6 +24,20 @@ function Takvim() {
 					<li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i>  FabLab</li>
 				</ul>
 				<p>Önemli girişimlere imza atmış konuşmacıların, her zaman anlatılanların dışına çıkacağı ve ardından AMA(istediğini sorabilirsin) formatında ilerleneceği ufkunuzu açabilecek bir etkinlik.</p>
+			</div>
+		</div>	<div class="row row-striped" style={{marginBottom:"10px"}}>
+			<div class="col-2 text-right">
+				<h3 class="display-10"><span class="badge badge-secondary">1-3</span></h3>
+				<h5>EYL</h5>
+			</div>
+			<div class="col-10">
+				<h5 class="text"><strong>FikrimİZ Ideathon</strong></h5>
+				<ul class="list-inline">
+				    <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Salı </li>
+					<li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 9:00 - 23:00 </li>
+					<li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Tarihi Havagazı Fabrikası </li>
+				</ul>
+				<p>İzmir Havagazı Fabrikası, Gençlik Yerleşkesi ve FikrimİZ önkuluçka merkezi Fikrimİz Ideathon etkinliği 01-02-03 Eylül tarihlerinde 60 gencin katılımı, ilham verici konuşmacılar, eğitimler ve gençlerin ekip çalışması ile fikir maratonunda projelerini jüriye sunacakları 3 günlük bir program ile gerçekleşecektir.</p>
 			</div>
 		</div>	
 	</div>
--- src/Views/Home/StartUp.js
+++ src/Views/Home/StartUp.js
@@ -28,7 +28,7 @@ function StartUp() {
           <h3 class="mb-0">FabLab</h3>
           <p class="card-text mb-auto">FabLab, üretim laboratuvarı anlamına gelen “Fabrication Laboratory”nin kısaltmasıdır. Aynı zamanda “Fabulous Laboratory” (Muhteşem Laboratuvar) biçiminde de açıklanan FabLab, piyasadaki üretim için geliştirilmiş yazılım ve süreçlere bağlı makine ve parçalardan oluşturulmuş bir çeşit atölyedir. MIT (Massachussets Institute of Technology) tarafından kurulmuş olan CBA'da (Center for Bits and Atoms) Prof. Neil Gershenfeld'in verdiği “How to do (almost) anything" dersinden yola çıkılarak geliştirilmiştir.
 </p>
-          <a href="#" class="stretched-link">Okuamaya devam et</a>
+          <a href="#" class="stretched-link">Okumaya devam et</a>
         </div>
      
     </div>
@@ -37,27 +37,48 @@ function StartUp() {
     <div class="col-md-4">
     <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
+                <a class="text-dark" href="#">b-fit</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">fitness</div>
+              <p class="card-text mb-auto">Kadınlara özel spor merkezi.</p>
+              <a href="#">devamını oku</a>
             </div>
           </div>
 
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
+                <a class="text-dark" href="#">Takawear</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">moda</div>
+              <p class="card-text mb-auto">Kadın giyim</p>
+              <a href="#">devamını oku</a>
               </div>
             </div>
+
+            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <h3 class="mb-0">
+                <a class="text-dark" href="#">Baydöner</a>
+              </h3>
+              <div class="mb-1 text-muted">Yiyecek ve içecek</div>
+              <p class="card-text mb-auto">Döner fast-food</p>
+              <a href="#">devamını oku</a>
+              </div>
+            </div>
+
+            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+            <div class="card-body d-flex flex-column align-items-start">
+              <h3 class="mb-0">
+                <a class="text-dark" href="#">Steamlab</a>
+              </h3>
+              <div class="mb-1 text-muted">Sterilizasyon</div>
+              <p class="card-text mb-auto">Gıda sterilizasyon ve işleme</p>
+              <a href="#">devamını oku</a>
+              </div>
+            </div>
+
           </div>
 
        
