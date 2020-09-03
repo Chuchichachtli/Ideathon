976e9c0470386df88caea5697233023f3370baef
kayacan <kayacan@mindbehind.com>
2020-09-03 13:30:55 +0300

    takvım updated

--- src/Components/Takvim.js
+++ src/Components/Takvim.js
@@ -13,34 +13,19 @@ function Takvim() {
 	<div class="container">
 		<div class="row row-striped" style={{marginBottom:"10px"}}>
 			<div class="col-2 text-right">
-				<h1 class="display-4"><span class="badge badge-secondary">23</span></h1>
-				<h2>OCT</h2>
+				<h3 class="display-10"><span class="badge badge-secondary">14</span></h3>
+				<h5>EKM</h5>
 			</div>
 			<div class="col-10">
-				<h3 class="text"><strong>Girişimciliğin Bilinmeyenleri Semineri</strong></h3>
+				<h5 class="text"><strong>Girişimciliğin Bilinmeyenleri Semineri</strong></h5>
 				<ul class="list-inline">
 				    <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Pazartesi</li>
 					<li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 - 14:00 </li>
 					<li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i>  FabLab</li>
 				</ul>
-				<p>Önemli girişimlere imza atmış konuşmacıların, her zaman anlatılanların dışına çıkacağı ve ardından AMA(istediğini sorabilirsin) formatında ilerleneceği ufkunuzu açaabilecek bir etkinlik.</p>
+				<p>Önemli girişimlere imza atmış konuşmacıların, her zaman anlatılanların dışına çıkacağı ve ardından AMA(istediğini sorabilirsin) formatında ilerleneceği ufkunuzu açabilecek bir etkinlik.</p>
 			</div>
-		</div>
-		<div class="row row-striped">
-			<div class="col-2 text-right">
-				<h1 class="display-4"><span class="badge badge-secondary">27</span></h1>
-				<h2>OCT</h2>
-			</div>
-			<div class="col-10">
-				<h3 class="text-uppercase"><strong>Operations Meeting</strong></h3>
-				<ul class="list-inline">
-				    <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Friday</li>
-					<li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
-					<li class="list-inline-item"><i class="fa fa-location-arrow" aria-hidden="true"></i> Room 4019</li>
-				</ul>
-				<p>Fablab aylık operasyon toplantısı.</p>
-			</div>
-		</div>
+		</div>	
 	</div>
 	
           </>
